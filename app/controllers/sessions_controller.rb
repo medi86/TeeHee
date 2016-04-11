d183cf1fa7b2949a0a1239dc5ac3a6e6e10df98d
Matthew Swan <swanm86@gmail.com>
2016-04-11 16:51:59 -0500

    Before fucking around

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :create
   def new
   end
 
   def create
+    require 'pry'
+    binding.pry
   end
 
   def destroy
+  end
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
