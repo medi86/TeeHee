fc0d4b34435f8add99a4290081b147a6385167f3
Dongmin Lee <medi8600@gmail.com>
2016-04-12 08:04:48 -0500

    add more API function that can pull other people's tweet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,31 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
+
+  # config = {
+  #   consumer_key: Rails.application.secrets.twitter_consumer_key,
+  #   consumer_secret: Rails.application.secrets.twitter_consumer_secret
+  # }
+
+  # client = Twitter::REST::Client.new(config)
+
   def new
+    config = {
+      consumer_key: Rails.application.secrets.twitter_consumer_key,
+      consumer_secret: Rails.application.secrets.twitter_consumer_secret
+    }
+
+    @client = Twitter::REST::Client.new(config)
+
+    def @client.get_all_tweets(user)
+      collect_with_max_id do |max_id|
+        options = {count: 200, include_rts: true}
+        options[:max_id] = max_id unless max_id.nil?
+        user_timeline(user, options)
+      end
+    end
+
   end
 
-  # a;lsdkfja;lsdjf;asdj;
   def create
   end
 
@@ -13,4 +35,11 @@ class SessionsController < ApplicationController
   def auth_hash
     request.env['omniauth.auth']
   end
+
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+
 end
